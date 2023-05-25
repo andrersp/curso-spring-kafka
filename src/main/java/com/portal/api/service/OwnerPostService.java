d0b899326c2b70d83d0cd2ea6222e22ec84c23d5
rspandre <rsp.assistencia@gmail.com>
2023-05-24 23:07:56 -0300

    start kafka

--- src/main/java/com/portal/api/service/OwnerPostService.java
+++ src/main/java/com/portal/api/service/OwnerPostService.java
@@ -1,7 +1,10 @@
 package com.portal.api.service;
 
+import org.springframework.stereotype.Service;
+
 import com.portal.api.dto.OwnerPostDTO;
 
+@Service
 public interface OwnerPostService {
 
     void postOwner(OwnerPostDTO newUser);
--- src/main/java/com/portal/api/service/OwnerPostServiceImpl.java
+++ src/main/java/com/portal/api/service/OwnerPostServiceImpl.java
@@ -1,10 +1,12 @@
 package com.portal.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.portal.api.client.CarPostStoreClient;
 import com.portal.api.dto.OwnerPostDTO;
 
+@Service
 public class OwnerPostServiceImpl implements OwnerPostService {
 
     @Autowired
